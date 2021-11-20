a8d2d6ea176aabdeb2efa06bcf322cc1f37d59f8
u-kai <76635578+u-kai@users.noreply.github.com>
2021-11-20 11:22:55 +0900

    clear insert_new_line

--- src/mods/do_parser.rs
+++ src/mods/do_parser.rs
@@ -1,10 +1,12 @@
+use std::fmt::format;
+
 use regex::Regex;
 
 use super::tag::tag::Tag;
 
 #[derive(Debug)]
 pub struct DOParser {
-    content:String
+    pub content:String
 }
 impl DOParser {
     pub fn new(content:String) -> Self {
@@ -14,18 +16,25 @@ impl DOParser {
         Tag::new("sheetData".to_string(),None,None)
     }
     pub fn fmt_content(&mut self) -> &Self {
-        &self.to_lf();
-        &self.fmt_do()
+        self.to_lf();
+        self.insert_new_line()
     }
-    fn fmt_do(&mut self)-> &Self {
-        let re = Regex::new(r">([^\n]).+").unwrap();
-        println!("{}",re.is_match(&self.content));
-        let capts = re.captures(&self.content).unwrap();
-        self.content = re.replace_all(&self.content, format!(">\n{}",&capts.get(0).unwrap().as_str()[1..])).to_string();
-        let re = Regex::new(r"([^\s]+)<").unwrap();
-        let capts = re.captures(&self.content).unwrap();
-        println!("{}",capts.get(0).unwrap().as_str());
-        self.content = re.replace_all(&self.content, format!("{}\n<",&capts.get(0).unwrap().as_str()[..&capts.get(0).unwrap().as_str().len()-1])).to_string();
+    fn insert_new_line(&mut self) -> &Self {
+        let re = Regex::new(r">([^\n<]*)<").unwrap();
+        loop {
+            if !re.is_match(&self.content){
+                break;
+            }
+            let capts = re.captures(&self.content).unwrap().get(1).unwrap().as_str();
+            if capts == "" {
+
+                self.content = re.replace(&self.content, ">\n<").to_string();
+            }else{
+
+            self.content = re.replace(&self.content, format!(">\n{}\n<",capts)).to_string();
+            }
+
+        }
         self
     }
     fn to_lf(&mut self) -> &Self {
@@ -34,4 +43,17 @@ impl DOParser {
         re.replace_all(&self.content, "\n");
         self
     }
+}
+#[cfg(test)]
+mod tests{
+    use crate::mods::do_parser::DOParser;
+
+#[test]
+fn it_w(){
+    let content = "<div><div>a</div><div>data</div></div>";
+    let mut dp = DOParser::new(content.to_string());
+    dp.fmt_content(); 
+    assert_eq!(dp.content,"<div>\n<div>\na\n</div>\n<div>\ndata\n</div>\n</div>")
+} 
+
 }
