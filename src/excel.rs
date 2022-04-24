0cc6c0a609ce27d98988c40ee5ad3f4e3d29d6fc
u-kai <76635578+u-kai@users.noreply.github.com>
2022-04-24 18:21:56 +0900

    pre shared_strings

--- src/excel.rs
+++ src/excel.rs
@@ -1,2 +1,3 @@
 pub mod cell;
+pub mod shared_strings;
 pub mod sheet;
---
+++ src/excel/shared_strings.rs
@@ -0,0 +1,63 @@
+//use crate::xml::nodes::node::XMLNode;
+
+//pub struct SharedStrings {
+//values: Vec<String>,
+//}
+
+//impl SharedStrings {
+//pub fn new(s: &str) -> Self {
+//let mut xml_node = XMLNode::from(s);
+//let sst = xml_node.nth_child(0).unwrap();
+
+//let vec = sst.search_nodes("si").unwrap();
+//println!("{:?}", vec);
+//let values = vec
+//.iter()
+//.map(|node| node.search_node("t").unwrap())
+//.collect::<Vec<_>>();
+//SharedStrings { values }
+//}
+//pub fn get_value(&self, index: usize) -> &str {
+//&self.values[index]
+//}
+//}
+
+//#[cfg(test)]
+//mod shared_strings_test {
+//use super::SharedStrings;
+
+//#[test]
+//fn get_value_test() {
+//let ss = SharedStrings::new(
+//r#"
+//<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+//<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" count="938" uniqueCount="265">
+//<si>
+//<t>詳細画面レイアウト</t>
+//<rPh sb="0" eb="2">
+//<t>ショウサイ</t>
+//</rPh>
+//<rPh sb="2" eb="4">
+//<t>ガメン</t>
+//</rPh>
+//<phoneticPr fontId="2"/>
+//</si>
+//<si>
+//<t>会社名</t>
+//<rPh sb="0" eb="3">
+//<t>カイシャメイ</t>
+//</rPh>
+//<phoneticPr fontId="2"/>
+//</si>
+//<si>
+//<t>タイトル</t>
+//<phoneticPr fontId="2"/>
+//</si>
+//</sst>
+//"#,
+//);
+//assert_eq!(ss.get_value(0), "詳細画面レイアウト");
+//assert_eq!(ss.get_value(1), "会社名");
+//assert_eq!(ss.get_value(2), "タイトル");
+//}
+//}
