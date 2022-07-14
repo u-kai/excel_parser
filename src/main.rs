6e3a7325a6736b8315f2e500575ff882b7fa6b6e
u-kai <76635578+u-kai@users.noreply.github.com>
2022-07-14 19:32:37 +0900

    add drawio project

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,14 @@
-use std::io::{stdout, Read, Write};
-use std::{net, time};
-
-use html::dom::Dom;
-
 use crate::excel::cells::cell::ECell;
 use crate::excel::excel::Excel;
 use crate::excel::file_operator::XLSXFile;
 use crate::excel::xmls::sheet::WorkSheet;
 
+mod drawio;
 mod excel;
 mod html;
 mod xml;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let time = time::Instant::now();
     let mut xlsx = XLSXFile::open("test_buf.xlsx");
     let mut excel = Excel::new(&mut xlsx);
     excel.read_sheet("term1");
@@ -22,7 +17,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     sheet1.set_cell(&cell);
     println!("{:?}", sheet1.get_all_cell());
     println!("{:?}", sheet1.to_xml());
-    println!("{:?}", time.elapsed());
     excel.save(sheet1);
     Ok(())
 }
